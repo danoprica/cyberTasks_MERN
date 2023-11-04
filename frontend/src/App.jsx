e201b5cb2a72668125a82292c209d19556116d31
danoprica <danutzoprica@yahoo.com>
2023-11-05 00:35:59 +0200

    Added header font in frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,30 +1,34 @@
 function App() {
 
   return (
-    <div className='App'>
-      <h1>Cybertasks</h1>
+    <>
+      <div className='App'>
+        <div className='container'>
+          <h1>Cybertasks</h1>
 
 
-      <div className='todos'>
-        <div className='todo'>
-          <div className='checkbox'></div>
-          <div className='text'>
-            cacat
+          <div className='todos'>
+            <div className='todo'>
+              <div className='checkbox'></div>
+              <div className='text'>
+                cacat
+              </div>
+              <div className='delete-todo'>X</div>
+            </div>
           </div>
-          <div className='delete-todo'>X</div>
+
+          <div className='todos'>
+            <div className='todo is-complete'>
+              <div className='checkbox'></div>
+              <div className='text'>
+                cacat
+              </div>
+              <div className='delete-todo'>X</div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className='todos'>
-        <div className='todo is-complete'>
-          <div className='checkbox'></div>
-          <div className='text'>
-            cacat
-          </div>
-          <div className='delete-todo'>X</div>
-        </div>
-      </div>
-    </div>
+    </>
   )
 }
 
