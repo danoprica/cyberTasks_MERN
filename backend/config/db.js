163f03630e87b82ae885667d6a6f89d445bff0fd
danoprica <danutzoprica@yahoo.com>
2023-11-04 12:53:19 +0200

    Connected to MongoDB, completed .gitignore file

---
+++ backend/config/db.js
@@ -0,0 +1,9 @@
+import mongoose from 'mongoose'
+import dotenv from 'dotenv'
+dotenv.config()
+
+const connectDB = mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("Connected to MongoDB"))
+  .catch(console.error)
+
+export default connectDB
