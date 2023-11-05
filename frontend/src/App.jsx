f56df32b45927345e2ac1e868a67ce9937c7eddc
danoprica <danutzoprica@yahoo.com>
2023-11-05 13:11:49 +0200

    Fetching tasks from backend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,34 +1,46 @@
+import {useEffect, useState} from "react";
+import axios from "axios";
+
+const API_URL = 'http://localhost:8000/api/tasks'
+
 function App() {
 
+  const [tasks, setTasks] = useState([])
+
+  useEffect(() => {
+    GetTasks()
+  }, [])
+
+  const GetTasks = () => {
+    axios
+        .get(API_URL)
+        .then((res) => {
+          console.log(res.data)
+          setTasks(res.data)
+        })
+        .catch((err) => {
+          console.log(err)
+        })
+  }
+
   return (
-    <>
-      <div className='App'>
-        <div className='container'>
-          <h1>Cybertasks</h1>
-
-
-          <div className='todos'>
-            <div className='todo'>
-              <div className='checkbox'></div>
-              <div className='text'>
-                cacat
+    <div className='App'>
+      <div className='container'>
+        <h1>Cybertasks</h1>
+        <div className='tasks'>
+          {tasks.map(task => (
+              <div className={'task ' + (task.complete ? 'is-complete': '')} key={task._id}>
+                <div className='checkbox'></div>
+                <div className='text'>
+                  { task.text }
+                </div>
+                <div className='delete-task'>X</div>
               </div>
-              <div className='delete-todo'>X</div>
-            </div>
-          </div>
+          ))}
 
-          <div className='todos'>
-            <div className='todo is-complete'>
-              <div className='checkbox'></div>
-              <div className='text'>
-                cacat
-              </div>
-              <div className='delete-todo'>X</div>
-            </div>
-          </div>
         </div>
       </div>
-    </>
+    </div>
   )
 }
 
