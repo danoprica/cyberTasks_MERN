8e34515816a8b6ff6c17e971263607486a6cfcf6
danoprica <danutzoprica@yahoo.com>
2023-11-05 20:55:37 +0200

    Deleted environment condition

--- backend/server.js
+++ backend/server.js
@@ -29,17 +29,7 @@ app.use('/api/tasks', taskRoutes)
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '/frontend/dist')))
-
-  app.get('*', (req, res) =>
-    res.sendFile(path.resolve(__dirname, '..', 'frontend', 'dist', 'index.html'))
-  )
-} else {
-  app.get('/', (req, res) => {
-    res.send('API is running...')
-  })
-}
+app.use(express.static(path.join(__dirname, '../frontend/dist')))
 
 app.use(notFound)
 app.use(errorHandler)
