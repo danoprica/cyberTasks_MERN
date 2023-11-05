e5cec7bb90cefe7d7e9de7e46c89ee028d3b6285
danoprica <danutzoprica@yahoo.com>
2023-11-05 19:24:09 +0200

    Production build requirments

--- backend/server.js
+++ backend/server.js
@@ -3,7 +3,8 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import taskRoutes from './routes/taskRoutes.js'
-import cors from "cors";
+import cors from 'cors';
+import * as path from 'path';
 dotenv.config()
 const port = process.env.PORT || 5000
 
@@ -17,12 +18,23 @@ app.use(cors({ origin: true, credentials: true }));
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
-app.get('/', (req, res) => {
-  res.send('API is running...')
-})
+
 
 //routes
 app.use('/api/tasks', taskRoutes)
+
+
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, '/frontend/build')))
+
+  app.get('*', (req, res) =>
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  )
+} else {
+  app.get('/', (req, res) => {
+    res.send('API is running...')
+  })
+}
 
 app.use(notFound)
 app.use(errorHandler)
