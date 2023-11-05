ad50ca14a77ab8b63c37c244b0ee71825c71bd05
danoprica <danutzoprica@yahoo.com>
2023-11-05 11:11:11 +0200

    Added cors and axios

--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,17 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import taskRoutes from './routes/taskRoutes.js'
+import cors from "cors";
 dotenv.config()
 const port = process.env.PORT || 5000
 
 connectDB
 
+// cors
+
 const app = express()
 
+app.use(cors({ origin: true, credentials: true }));
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 
@@ -17,6 +21,7 @@ app.get('/', (req, res) => {
   res.send('API is running...')
 })
 
+//routes
 app.use('/api/tasks', taskRoutes)
 
 app.use(notFound)
